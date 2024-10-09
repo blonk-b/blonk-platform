1a3c83ab631b34e3db100d25287ac985bca96e67
Leandro Gavidia Santamaria <leandro.d3v@gmail.com>
2024-10-08 23:36:51 -0400

    feat: add utils module

---
+++ src/utils/consts.rs
@@ -0,0 +1,6 @@
+use solana_sdk::{pubkey, pubkey::Pubkey};
+
+pub const MULTISIG_PUBKEY: Pubkey = pubkey!("F52sD3r8tNSViSR4A2skWNzUKKM56nV6PivW5L4YASdi");
+pub const SQUADS_PROGRAM_ID: Pubkey = pubkey!("SMPLecH534NA9acpos4G6x7uf3LWbCAwZQE9e8ZekMu");
+pub const RPC: &str =
+        "https://mainnet.helius-rpc.com/?api-key=22a5a327-77fd-4021-b6d3-2d1ccb26fb18";
